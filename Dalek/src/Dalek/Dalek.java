3112f4b4504ae23715709e2d8c5aaa02f9638cfd
TheMHMoritz3 <mhmoritz3@gmail.com>
2015-10-24 18:30:06 +0200

    Add Project
    New Files:
    Dalek.java
    New Project Dalek
    
    Packages:
    Main Package Dalek
    Only for the Dalek.java

---
+++ Dalek/src/Dalek/Dalek.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Dalek;
+
+import robocode.AdvancedRobot;
+
+/**
+ *
+ * @author Moritz
+ */
+public class Dalek extends AdvancedRobot {
+
+    @Override
+    public void run() {
+        
+    }
+    
+}
