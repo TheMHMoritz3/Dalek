0ddb4a0fd9f3a853d2bfb073b4bdfd3789befbd5
TheMHMoritz3 <mhmoritz3@gmail.com>
2015-10-27 17:20:18 +0100

    JavaDoc
    Adds JavaDoc to the Shooting Factory.

--- Dalek/src/Dalek/Shooting/ShootingFactory.java
+++ Dalek/src/Dalek/Shooting/ShootingFactory.java
@@ -10,6 +10,9 @@ package Dalek.Shooting;
  * @author Moritz
  */
 public class ShootingFactory {
+    /**
+     * CTor
+     */
     public ShootingFactory()
     {
         UsedTactic=Tactics.LinearTargeting;
@@ -19,7 +22,10 @@ public class ShootingFactory {
         //Statistics = new StatisticsFactory();
         BulletPower = 1;
     }
-	
+    
+    /**
+     * Is called on every shoot and decides about a tactic change.
+     */
     public void decideAboutTactics()
     {
         /*if(Statistics.getBulletStatisticsBulletCount()>5)
@@ -31,7 +37,10 @@ public class ShootingFactory {
             }
         }*/
     }
-	
+    
+    /**
+     * Changes the Tactics with the support of the StaticsticsFactory.
+     */
     private void changeTactics()
     {
         /*if(Statistics.getEnemyMovementFromMovementStatistics()==EnemyMovementTactics.LineMovement)
@@ -87,14 +96,23 @@ public class ShootingFactory {
 
         //Statistics.resetBulletCount();
     }
-	
+    
+    /**
+     * Calculates the gunpower.
+     * @param myHealth The Health of the Robot.
+     */
     public void calculateGunPower(double myHealth)
     {
         BulletPower=Math.min(myHealth, BulletPower);
         TargetingLinear.setBulletPower(BulletPower);
         //TargetingCircular.setBulletPower(BulletPower);
     }
-	
+    
+    /**
+     * Returns the value in which angle the gun schould turn.
+     * This method dicides with the help of the Tactics enum.
+     * @return Angle of the gun.
+     */
     public double turnGunTo()
     {
         switch(UsedTactic)
@@ -114,41 +132,73 @@ public class ShootingFactory {
         }
         return 0.0;
     }
-	
+    
+    /**
+     * Is need to be set. Sets all the varibles of the RobotScannedEvent that are needed.
+     * @param distance Distance to the enemy.
+     * @param headingRadinas Heading of the enemy in Radians.
+     * @param robotVelocety Enemys Velocety
+     * @param bearingRadians Bearing of the enemy in Radians.
+     */
     public void setDataOfEvent(double distance, double headingRadinas, double robotVelocety, double bearingRadians)
     {
         TargetingLinear.setDataOfEvent(distance, headingRadinas, robotVelocety, bearingRadians);
         //TargetingCircular.setDataOfEvent(distance, headingRadinas, robotVelocety, bearingRadians);
     }
-
+    
+    /**
+     * Need to be set. Sets Daleks Position in all Targeting classes.
+     * @param MyPosX Daleks Position X
+     * @param MyPosY Daleks Position Y
+     */
     public void setMyPosition(double MyPosX, double MyPosY)
     {
         TargetingLinear.setMyPosition(MyPosX, MyPosY);
         //TargetingCircular.setOwnXAndY(MyPosX, MyPosY);
     }
-
+    
+    /**
+     * Is need to be set. Sets the size of the Battlefield
+     * @param XSize Maximum X-Value of the Battlefield
+     * @param YSize Maximum Y-Value of the Battlefield
+     */
     public void setSizeOfBattlefield(double XSize, double YSize)
     {
         TargetingLinear.setSizeOfBattlefield(XSize, YSize);
         //TargetingCircular.setBattleFieldWidthAndHeight(XSize, YSize);
     }
-
+    
+    /**
+     * Is need to be set. Sets the Utilitys of the Dalek.
+     * @param myGunHeadingRadiance Daleks Gun Heading Radians.
+     * @param myHeadingRadians Daleks Heading Radians.
+     */
     public void setMyUtilitys(double myGunHeadingRadiance, double myHeadingRadians)
     {
         TargetingLinear.setMyUtilitys(myGunHeadingRadiance, myHeadingRadians);
         //TargetingCircular.setMyUtilitys(myHeadingRadians,myGunHeadingRadiance);
     }
-
+    
+    /**
+     * Returns the BulletPower.
+     * @return Bullet Power
+     */
     public double getBulletPower()
     {
         return BulletPower;
     }
-
+    
+    /**
+     * Adds a Missed Bullet. Will be Replaced.
+     */
     public void addMissedBullet()
     {
         //Statistics.addMissedBulletToBulletStatistics();
     }
-
+    
+    /**
+     * Adds a Hit Bullet. Will be Replaced.
+     */
     public void addHitBullet()
     {
         //Statistics.addHitBulletToBulletStatistics();
